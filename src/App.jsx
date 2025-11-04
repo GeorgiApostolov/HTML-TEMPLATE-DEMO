6df7edd21727366f6ca31e180c0e8be95ffb07e6
GeorgiApostolov <74099975+GeorgiApostolov@users.noreply.github.com>
2025-11-04 16:50:43 +0200

    Update jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import BigFeaturesLeft from "./components/BigFeaturesLeft.jsx";
-import BigFeaturesRight from "./components/BigFeaturesRight.jsx";
+import BigFeatures from "./components/BigFeatures.jsx";
 import Blog from "./components/Blog.jsx";
 import Contact from "./components/Contact.jsx";
 import CounterParallax from "./components/CounterParallax.jsx";
 import Features from "./components/Features.jsx";
 import Footer from "./components/Footer.jsx";
 import Header from "./components/Header.jsx";
-import HomeParallax from "./components/HomeParallax.jsx";
+import WorkProcessSection from "./components/WorkProcessSection.jsx";
 import Preloader from "./components/Preloader.jsx";
 import PricingPlans from "./components/PricingPlans.jsx";
 import Testimonials from "./components/Testimonials.jsx";
@@ -29,13 +28,25 @@ function App() {
       <Features />
       {/* ***** Features Small End ***** */}
       {/* ***** Features Big Item Start ***** */}
-      <BigFeaturesLeft />
+      <BigFeatures
+        title="Let’s discuss about you project"
+        content="Nullam sit amet purus libero. Etiam ullamcorper nisl ut augue
+                blandit, at finibus leo efficitur. Nam gravida purus non sapien
+                auctor, ut aliquam magna ullamcorper."
+      />
       {/* ***** Features Big Item End ***** */}
       {/* ***** Features Big Item Start ***** */}
-      <BigFeaturesRight />
+      <BigFeatures
+        title="We can help you to grow your business"
+        content="Aenean pretium, ipsum et porttitor auctor, metus ipsum iaculis
+                nisi, a bibendum lectus libero vitae urna. Sed id leo eu dolor
+                luctus congue sed eget ipsum. Nunc nec luctus libero. Etiam quis
+                dolor elit."
+        reverse
+      />
       {/* ***** Features Big Item End ***** */}
       {/* ***** Home Parallax Start ***** */}
-      <HomeParallax />
+      <WorkProcessSection />
       {/* ***** Home Parallax End ***** */}
       {/* ***** Testimonials Start ***** */}
       <Testimonials />
